801a72c95d1411254c8efa77a5f168fe1ae45398
abolfazl_khosrojerdi <abolfazlkhosrojerdi@outlook.com>
2024-05-12 21:54:20 +0330

    str to int modification for input

--- factorial.py
+++ factorial.py
@@ -1,5 +1,5 @@
 #this code calculate factorial for a whole number
-inp = input('your number to calculate fact:')
+inp = int(input('your number to calculate fact:'))
 factorial = 1
 for n in range(1, inp+1):
     factorial *= n
