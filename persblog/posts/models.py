53d556a8ea330143de81068d5623fcd451171488
abolfazl_khosrojerdi <abolfazlkhosrojerdi@outlook.com>
2024-05-24 22:52:32 +0330

    create first post class

--- persblog/posts/models.py
+++ persblog/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
+class Post(models.Model):
+    title  = models.CharField(max_length=50)
+    author = ''
+    is_enable = models.BooleanField(default=True)
+    publish_date = models.DateTimeField()
+    created_date = models.DateTimeField()
+    modified_date = models.DateTimeField()
+    updated_date = models.DateTimeField()
+    text = models.TextField()
+
 # Create your models here.
--- persblog/users/apps.py
+++ persblog/users/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthenConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authen'
+    name = 'users'
