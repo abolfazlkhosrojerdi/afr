dd7bae36f9453c6963e086ead56ce27f5514628e
abolfazl_khosrojerdi <abolfazlkhosrojerdi@outlook.com>
2024-05-26 21:16:46 +0330

    persblog is in progress ...

--- persblog/posts/admin.py
+++ persblog/posts/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
 from .models import Post
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'publish_date', 'created_date','is_enable','updated_date']
+admin.site.register(Post,PostAdmin)
 # Register your models here.
--- persblog/posts/models.py
+++ persblog/posts/models.py
@@ -5,10 +5,10 @@ class Post(models.Model):
     title  = models.CharField(max_length=50)
     author = ''
     is_enable = models.BooleanField(default=True)
-    publish_date = models.DateTimeField()
-    created_date = models.DateTimeField()
-    modified_date = models.DateTimeField()
-    updated_date = models.DateTimeField()
-    text = models.TextField()
+    publish_date = models.DateTimeField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    text = models.TextField(blank=True)
 
 # Create your models here.
